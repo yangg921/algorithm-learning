d25a5570e1bc473099e79f83af06c6d673e2e15b
yang <2300223450@qq.com>
2022-07-02 14:09:58 +0800

    根据优先级运算

--- pages/api/expression.ts
+++ pages/api/expression.ts
@@ -1,21 +1,24 @@
-export default function diffWaysToCompute(expression: string):number[] | string[]{
+export default function diffWaysToCompute(expression: string): number[] |string[] {
     const len = expression.length
     const arr:number[] = []
     for(let i = 0;i<=len;i++){
         let left:number[] | string[]
         let right:number[] | string[]
         if(['*','-','+'].includes(expression[i])){
-            const s = expression[i]
             left = diffWaysToCompute(expression.substring(0,i)) as number[]
             right = diffWaysToCompute(expression.substring(i+1)) as number[]
             for (let j of left){
                 for (let k of right){
-                    if(s === '*') {
-                        arr.push(j*k)
-                    }if(s === '-') {
-                        arr.push(j-k)
-                    }if(s === '+') {
-                        arr.push(j+k)
+                    switch(expression[i]){
+                        case '+':
+                            arr.push(Number(j)+Number(k))
+                            break
+                        case '-':
+                            arr.push(j-k)
+                            break
+                        case '*':
+                            arr.push(j*k)
+                            break
                     }
                 }
             }
